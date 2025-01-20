a52d0b68bcca0dcae984cdc5eff9daeb3985e80d
DerekSave <workderekceo@gmail.com>
2025-01-20 15:18:47 -0400

    Setup the proyect

---
+++ Frotend-Caja-disimulada/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Welcome to My React App!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
